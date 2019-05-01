8be6482ddd53e3dc9769f01eb1400301515d5eb0
SetTheCurve <setthecurveltdco@gmail.com>
2019-05-01 00:08:58 -0500

    Updated breakpoints for page content and open/close functionality on aside menu

---
+++ _nuxt/7799789b07b952b4a14a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{299:function(n,t,e){"use strict";e.r(t);var c={components:{}},o=e(7),component=Object(o.a)(c,function(){var n=this.$createElement;return(this._self._c||n)("section",{staticClass:"container"})},[],!1,null,null,null);t.default=component.exports}}]);
